75f51be519742ac397d364fe715d9d38bd7d6c33
flo-richard <flo.richard02@gmail.com>
2022-10-31 09:47:49 +0100

    Update for dashboard

--- app.py
+++ app.py
@@ -10,36 +10,45 @@ Model = ScoringModel()
 
 
 @app.post('/getPrediction')
-async def get_prediction(info : Request):
-    req_info = await info.json()
-    df = pd.DataFrame([req_info])
+async def get_prediction(info : int):
+
+    id = await info.json()
+
+    if id not in Lodel.data.index:
+        return {
+            'Status': 'Error',
+            'Message': 'Error: Unknown ID'
+        }
+    
+    else:
+
+        df = Model.data.loc[id]
 
 
+        print('Preprocessing...')
+        df = Model.preprocessing(df)
+        print('Preprocessing ok')
 
-    print('Preprocessing...')
-    df, req_info = Model.preprocessing(df, req_info)
-    print('Preprocessing ok')
+        print('Computing prediction...')
+        prediction, probas = Model.predict(df)        
+        print('Prediction :', prediction)
 
-    print('Computing prediction...')
-    prediction, probas = Model.predict(df)        
-    print('Prediction :', prediction)
+        print('Computing explainer...')
+        expl_details_map, expl_details_list = Model.explain_prediction(df)
+        print('Done')
 
-    print('Computing explainer...')
-    expl_details_map, expl_details_list = Model.explain_prediction(df)
-    print('Done')
+        distributions = Model.return_distributions(expl_details_map)
+        print('Done')
 
-    distributions = Model.return_distributions(expl_details_map)
-    print('Done')
-
-    return {
-        'Status': 'Success',
-        'Prediction': int(prediction),
-        'Prediction probabilities': probas,
-        'User info': req_info,
-        'Explainer map': expl_details_map.to_dict('list'),
-        'Explainer list': expl_details_list,
-        'Distributions': distributions
-    }
+        return {
+            'Status': 'Success',
+            'Prediction': int(prediction),
+            'Prediction probabilities': probas,
+            'User info': df.to_dict(),
+            'Explainer map': expl_details_map.to_dict('list'),
+            'Explainer list': expl_details_list,
+            'Distributions': distributions
+        }
 
 if __name__ == '__main__':
     main()
--- model.py
+++ model.py
@@ -10,11 +10,13 @@ import xgboost as xgb
 class ScoringModel:
     def __init__(self):
         
-
         self.list_features = pickle.load(open('Pickles/features.pkl', 'rb'))
         self.all_features = self.list_features['all_features']
         self.continuous_features = self.list_features['continuous_features']
-        self.categorical_features = self.list_features['categorical_features']        
+        self.categorical_features = self.list_features['categorical_features']     
+
+        self.data = pd.read_csv('Data/client_data_sample.csv', index_col=0)
+        self.data = self.data.drop([i for i in self.data.columns if i not in self.all_features], axis=1)
 
         self.model = pickle.load(open('Pickles/xgb.pkl', 'rb')) # load trained model
 
@@ -28,7 +30,7 @@ class ScoringModel:
 
 
 
-    def preprocessing(self, df: pd.DataFrame, req_info: dict):
+    def preprocessing(self, df: pd.DataFrame):
         """Preprocess data (feature engineering, imputers, label encoders"""
 
         # Feature engineering
@@ -37,9 +39,7 @@ class ScoringModel:
         df['ANNUITY_INCOME_RATE'] = df['AMT_ANNUITY'] / df['AMT_INCOME_TOTAL']
         df['PAYMENT_RATE'] = df['AMT_ANNUITY'] / df['AMT_CREDIT']
 
-        req_info['INCOME_CREDIT_RATE'] = df['INCOME_CREDIT_RATE'].values[0]
-        req_info['ANNUITY_INCOME_RATE'] = df['ANNUITY_INCOME_RATE'].values[0]
-        req_info['PAYMENT_RATE'] = df['PAYMENT_RATE'].values[0]
+        df['DAYS_EMPLOYED'] = - df['DAYS_EMPLOYED'] 
 
         # Imputers
 
@@ -92,65 +92,3 @@ class ScoringModel:
             distributions_to_plot[i] = self.distributions[i]
             
         return distributions_to_plot
-        
-
-
-# list = [
-#     'Self-employed',
-#     'School',
-#     'University',
-#     'Kindergarten',
-#     'Government',
-#     'Security Ministries',
-#     'Legal Services',
-#     'Postal',
-#     'Military',
-#     'Police',
-#     'Security',
-#     'Services',
-#     'Religion',
-#     'Medicine',
-#     'Emergency',
-#     'Electricity',
-#     'Construction',
-#     'Realtor',
-#     'Housing',
-#     'Hotel',
-#     'Bank',
-#     'Insurance',
-#     'Mobile',
-#     'Telecom',
-#     'Culture',
-#     'Advertising',
-#     'Agriculture',
-#     'Restaurant',
-#     'Cleaning',
-#     'Business Entity Type 1',
-#     'Business Entity Type 2',
-#     'Business Entity Type 3',
-#     'Transport: type 1',
-#     'Transport: type 2',
-#     'Transport: type 3',
-#     'Transport: type 4',
-#     'Trade: type 1',
-#     'Trade: type 2',
-#     'Trade: type 3',
-#     'Trade: type 4',
-#     'Trade: type 5',
-#     'Trade: type 6',
-#     'Trade: type 7',
-#     'Industry: type 1',
-#     'Industry: type 2'
-#     'Industry: type 3',
-#     'Industry: type 4',
-#     'Industry: type 5',
-#     'Industry: type 6'
-#     'Industry: type 7',
-#     'Industry: type 8',
-#     'Industry: type 9',
-#     'Industry: type 10',
-#     'Industry: type 11',
-#     'Industry: type 12',
-#     'Industry: type 13',
-#     'Other'
-# ]
